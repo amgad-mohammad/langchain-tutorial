08f2b2a32f60a74c303eda1e9610f3b0604e14cb
Amgad_Mohammed <amgad.mohammed@siliconexpert.com>
2026-05-21 12:22:55 +0300

    using chain with RunnableSequence

--- chains.py
+++ chains.py
@@ -3,6 +3,7 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence
 
 
 def chain_invocation_using_pipe_operator():
@@ -27,7 +28,27 @@ def chain_invocation_using_pipe_operator():
     llm_response= chain.invoke({'framework':user_input})
     print(llm_response)
 
+def chain_invocation_using_runnable_sequence():
+    # TASK-1
+    user_input = input("Enter the framework name:")
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", 'You are a helpful assistant for answering technology questions'),
+         ("human", 'Who is the creator of {framework}?')  # we can use human or user
+        ]
+    )
 
+    # TASK-2
+    ollama_llm =ChatOllama(model='qwen2.5:3b',temperature=0)
+
+    #Task-3
+    #StrOutputParser : parses the output by extracting the content from it (without it, we do that by using .content)
+    str_output_parser= StrOutputParser()
+
+    # creating a chain using pipe(|) operator (LCEL:Lang chain Expression Language)
+    chain= RunnableSequence(prompt_template,ollama_llm,str_output_parser)
+    #print(chain)
+    llm_response= chain.invoke({'framework':user_input})
+    print(llm_response)
 
 
 def manual_invocation():
@@ -43,4 +64,4 @@ def manual_invocation():
     print(response.content)
 
 if __name__ == '__main__':
-    chain_invocation_using_pipe_operator()
+    chain_invocation_using_runnable_sequence()
